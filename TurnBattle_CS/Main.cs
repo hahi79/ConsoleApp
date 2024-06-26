6e768680b83b0dd7c453239805f9b85bac8e7613
長谷川浩 <hasegawa@alfasystem.co.jp>
2024-06-26 12:48:45 +0900

    TurnBattle_CS プロジェクトを登録

---
+++ TurnBattle_CS/Main.cs
@@ -0,0 +1,62 @@
+﻿//======================================
+//      ターン制バトル メイン
+//======================================
+using Utility = GP2.Utility;
+using Character = TurnBattle_CS.Character;
+using TurnBattle = TurnBattle_CS.TurnBattle;
+using Command = TurnBattle_CS.Command;
+using CommandUI = TurnBattle_CS.CommandUI;
+
+class TurnBattleMain
+{
+    static int Main()
+    {
+        Utility.InitRand();
+        Character player = new Character(
+            100,        // HP
+            15,         // MP
+            30,         // 攻撃力
+            "ゆうしゃ", // 名前
+            ""          // アスキーアート
+            );
+        Character boss = new Character(
+            255,        // HP
+             0,         // MP
+             50,        // 攻撃力
+             "まおう",  // 名前
+            "　　Ａ＠Ａ\n"+   // アスキーアート
+            "ψ（▼皿▼）ψ"
+            );
+        Character zako = new Character(
+            3,         // HP
+            0,         // MP
+            2,         // 攻撃力
+            "スライム",// 名前
+            "／・Д・＼\n"+ // アスキーアート
+            "～～～～～"
+            );
+        TurnBattle btl = new TurnBattle(player, boss);
+
+        btl.Start();
+        btl.Intro();
+        bool isEnd = false;
+        Command cmd;
+        while (true)
+        {
+            cmd = CommandUI.GetPlayerCommand(btl);
+            isEnd=btl.ExecPlayerTurn(cmd);
+            if(isEnd)
+            {
+                break;
+            }
+            cmd = CommandUI.GetEnemyCommand();
+            isEnd = btl.ExecEnemyTurn(cmd);
+            if (isEnd)
+            {
+                break;
+            }
+            btl.NextTurn();
+        }
+        return 0;
+    }
+}
---
+++ TurnBattle_CS/Utility.cs
@@ -0,0 +1,72 @@
+﻿//======================================
+//      ユーティリティ
+//======================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP2
+{
+    enum Key
+    {
+        ARROW_U = ConsoleKey.UpArrow,
+        ARROW_D = ConsoleKey.DownArrow,
+        ARROW_L = ConsoleKey.LeftArrow,
+        ARROW_R = ConsoleKey.RightArrow,
+        DECIDE = ConsoleKey.Enter,
+    }
+
+    static class Utility
+    {
+        public const string EscBLACK   = "\x1b[30m";
+        public const string EscRED     = "\x1b[31m";
+        public const string EscGREEN   = "\x1b[32m";
+        public const string EscYELLOW  = "\x1b[33m";
+        public const string EscBLUE    = "\x1b[34m";
+        public const string EscMAZENTA = "\x1b[35m";
+        public const string EscCYAN    = "\x1b[36m";
+        public const string EscWHITE   = "\x1b[37m";
+        public const string EscDEFAULT = "\x1b[39m";
+
+        private static Random s_rand = new Random();
+        // 乱数初期化
+        public static void InitRand()
+        {
+            //s_rand = new Random();
+        }
+        // 乱数取得
+        public static int GetRand(int max)
+        {
+            return s_rand.Next(max);
+        }
+        // キー入力待ち
+        public static void WaitKey()
+        {
+            Console.ReadKey(true);
+        }
+        // キー取得
+        public static Key GetKey()
+        {
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            return (Key)info.Key;
+        }
+        // 画面クリア
+        public static void ClearScreen()
+        {
+            Console.Clear();
+        }
+        // printf()関数
+        public static void Printf(string fmt,params Object[] list)
+        {
+            string tmp = string.Format(fmt, list);
+            Console.Write(tmp);
+        }
+        // putchar()関数
+        public static void Putchar(Char c)
+        {
+            Console.Write(c);
+        }
+    } // class Utility
+} // namespace
