d63db270ebea6c9daebcba120196d17799cf5606
長谷川浩 <hasegawa@alfasystem.co.jp>
2024-07-02 17:24:00 +0900

    DotEatGame プロジェクトの登録

---
+++ DotEatGame/Character.cpp
@@ -0,0 +1,21 @@
+//======================================
+//	ドットイートゲーム　Character
+//======================================
+#include "Character.h"
+#include "Vector2.h"
+#include "Utility.h"  // GetKey()
+
+// キャラクター初期化
+void InitCharacter(Character* ch, Chara id, Vector2 initPos)
+{
+	ch->id = id;
+	ch->pos = initPos;
+	ch->lastPos = initPos;
+}
+
+// キャラクター移動
+void MoveCharacter(Character* ch, Vector2 newPos)
+{
+	ch->lastPos = ch->pos;
+	ch->pos = newPos;
+}
---
+++ DotEatGame/StageMoveCharacter.h
@@ -0,0 +1,11 @@
+//======================================
+//	ドットイートゲーム キャラクタ移動
+//======================================
+#ifndef ___STAGE_MOVE_CHARACTER_H
+#define ___STAGE_MOVE_CHARACTER_H
+#include "Stage.h"
+
+void MovePlayer(Stage* stage);
+void MoveMonsters(Stage* stage);
+
+#endif //  ___STAGE_MOVE_CHARACTER_H
