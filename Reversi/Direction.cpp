0e59c1d26599474a5a7bbf37dc16d433ef8d6b95
hahi <hasegawa@alfasystem.co.jp>
2024-08-27 17:41:45 +0900

    Direction.cpp 修正

--- Reversi/Direction.cpp
+++ Reversi/Direction.cpp
@@ -7,14 +7,14 @@
 
 // ８方向のベクター取得
 static Vector2 dirVector2[] = {
-	{0,-1}, //DIR_UP
-	{-1,-1},//DIR_UP_LEFT
-	{-1,0 },//DIR_LEFT
-	{-1,1},	//DIR_DOWN_LEFT,
-	{0,1},  //DIR_DOWN,
-	{1,1},  //DIR_DOWN_RIGHT,
-	{1,0},  //DIR_RIGHT,
-	{1,-1}, //DIR_UP, RIGHT,
+	{ 0,-1},  //DIR_UP
+	{-1,-1},  //DIR_UP_LEFT
+	{-1, 0},  //DIR_LEFT
+	{-1, 1},  //DIR_DOWN_LEFT,
+	{ 0, 1},  //DIR_DOWN,
+	{ 1, 1},  //DIR_DOWN_RIGHT,
+	{ 1, 0},  //DIR_RIGHT,
+	{ 1,-1},  //DIR_UP, RIGHT,
 };
 
 Vector2 GetDirVector2(DIRECTION d)
