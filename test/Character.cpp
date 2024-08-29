ed120d0ea0e807c9193e0aa3e6b437dafa5ea2dd
hahi <hasegawa@alfasystem.co.jp>
2024-08-29 18:07:49 +0900

    色替えのレートを変更

--- test/Character.cpp
+++ test/Character.cpp
@@ -108,10 +108,10 @@ void SetEraseAa(Character* ch)
 static void setColor(Character* ch)
 {
 	float rate = (float)ch->hp / ch->maxHp;
-	if (rate < 0.3f) {
+	if (rate < 0.2f) {
 		printf(EscRED);
 	}
-	else if (rate < 0.5f) {
+	else if (rate < 0.3333f) {
 		printf(EscYELLOW);
 	}
 	else {
